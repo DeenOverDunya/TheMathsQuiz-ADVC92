0bd1dddc564c2449512ea908503cf681b60a4793
DeenOverDunya <104869003+DeenOverDunya@users.noreply.github.com>
2022-07-10 21:38:34 +0100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDeQ0aaMbLPXJhFUoXTW0J63LZJbsNea1g",
+    authDomain: "let-s-chat-ad2c7.firebaseapp.com",
+    databaseURL: "https://let-s-chat-ad2c7-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-ad2c7",
+    storageBucket: "let-s-chat-ad2c7.appspot.com",
+    messagingSenderId: "353512085464",
+    appId: "1:353512085464:web:334e8350fcb274ee0c84ad"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
